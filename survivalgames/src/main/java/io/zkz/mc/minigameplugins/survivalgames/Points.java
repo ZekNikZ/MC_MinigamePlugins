044da1dfaed300f8f707f04854f86c56fbf9c22c
Matthew McCaskill <mattrmccaskill@gmail.com>
2022-09-10 10:51:22 -0500

    change scoring of SG

--- survivalgames/src/main/java/io/zkz/mc/minigameplugins/survivalgames/Points.java
+++ survivalgames/src/main/java/io/zkz/mc/minigameplugins/survivalgames/Points.java
@@ -1,6 +1,8 @@
 package io.zkz.mc.minigameplugins.survivalgames;
 
 public class Points {
-    public static int SURVIVAL = 40;
-    public static int PLAYER_KILL = 50;
+//    public static int SURVIVAL = 40;
+//    public static int PLAYER_KILL = 50;
+    public static int SURVIVAL = 20;
+    public static int PLAYER_KILL = 25;
 }
